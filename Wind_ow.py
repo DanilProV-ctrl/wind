f4c27c1e2f1c958a9528f2d38f03120603e3bf3f
DanilProV-ctrl <ProVDanil2019@yandex.ru>
2019-11-24 19:10:16 +1000

    Testok

---
+++ Wind_ow.py
@@ -0,0 +1,38 @@
+import os
+
+name = input('Скажи что выбрать(HL;LH)')
+
+if name == "LH":
+    print('Goodbyes')
+    os.rename("C:\Games","C:\Craked")
+    os.rename("C:\Program Files (x86)\Yandex","C:\Program Files (x86)\Googles")
+
+    what = input("did you look at my creation?(yes,no)")
+if what == "yes":
+        print("Ok ;)")
+elif what == "no":
+            print("disk c")
+            print("goodbyes")
+else:
+    print("stupid")
+
+print("good,bye")
+
+if name == "HL":
+    print("running LH...")
+    os.rename("C:\Games","C:\Craked")
+    os.rename("C:\Program Files (x86)\Yandex","C:\Program Files (x86)\Googles")
+
+    what = input("did you look at my creation?(yes,no)")
+    if what == "yes":
+        print("Ok ;)")
+elif what == "no":
+            print("disk c")
+
+
+                
+    
+
+
+
+
